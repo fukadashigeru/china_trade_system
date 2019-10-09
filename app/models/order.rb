27d7b5a3db58f9bed9d23c934aa1006245ea3fe4
fukadashigeru <sakusaku721@gmail.com>
2019-10-09 22:27:10 +0900

    assign_attributesに変更、usersの追加が既に登録済みの場合は登録しないようにする

--- app/models/order.rb
+++ app/models/order.rb
@@ -12,7 +12,7 @@ class Order < ApplicationRecord
     csv_utf8 = Kconv.toutf8(csv_text)
     CSV.parse(csv_utf8, headers: true, liberal_parsing: true) do |row|
       obj = find_by(trade_no: row["取引ID"]) || new
-      temp = {
+      obj.assign_attributes(
         quantity: row["受注数"],
         price: row["価格"],
         trade_no: row["取引ID"],
@@ -22,19 +22,19 @@ class Order < ApplicationRecord
         phone: row["電話番号"],
         color_size: row["色・サイズ"],
         customer_remark: row["連絡事項"]
-      }
-      obj.attributes = temp.slice(*updatable_attributes)
+      )
       # 発注者（日本人）と買付担当（中国人）のidを保存
       obj.update(retailer_id: current_user.id, china_buyer_id: china_buyer_id)
       obj.save!
       # 発注者（日本人）とのリレーションを保存
-      obj.users << current_user
+      obj.users << current_user unless obj.users.include?(current_user)
       # 買付担当（中国人）とのリレーションを保存
-      obj.users << User.find(china_buyer_id)
+      china_buyer = User.find(china_buyer_id)
+      obj.users << china_buyer unless obj.users.include?(china_buyer)
     end
   end
 
-  def self.updatable_attributes
-    [:quantity,:price,:trade_no,:customer_name,:postal,:address,:phone,:color_size,:customer_remark]
-  end
+  # def self.updatable_attributes
+  #   [:quantity,:price,:trade_no,:customer_name,:postal,:address,:phone,:color_size,:customer_remark]
+  # end
 end
