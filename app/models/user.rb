dee391df179df9d7ff721752b68b17d20d1b34df
fukadashigeru <sakusaku721@gmail.com>
2019-10-15 00:47:28 +0900

    import部分一部のupdateをassign_attributeに入れ込む_OrderとUserのアソシエーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,5 +7,7 @@ class User < ApplicationRecord
   china_buyer: 0, orderer: 1
   }
   has_many :user_orders
-  has_many :orders, through: :user_orders
+  # has_many :orders, through: :user_orders
+  has_many :japanese_retailer_orders, class_name: 'Order', :foreign_key => 'japanese_retailer_id'
+  has_many :chinese_buyer_orderss, class_name: 'Order', :foreign_key => 'chinese_buyer_id'
 end
