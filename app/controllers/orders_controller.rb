c52be44f61ecc62920b25edf438eab2c33553526
fukadashigeru <sakusaku721@gmail.com>
2019-11-01 01:47:44 +0900

    add form function for taobao_color_size

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,7 +5,7 @@ class OrdersController < ApplicationController
   # GET /orders
   # GET /orders.json
   def index
-    @orders = current_user.japanese_retailer_orders
+    @orders = current_user.japanese_retailer_orders.order(id: "ASC")
   end
 
   # GET /orders/1
@@ -20,6 +20,7 @@ class OrdersController < ApplicationController
 
   # GET /orders/1/edit
   def edit
+    @order = current_user.japanese_retailer_orders.find(params[:id])
   end
 
   # POST /orders
@@ -41,15 +42,9 @@ class OrdersController < ApplicationController
   # PATCH/PUT /orders/1
   # PATCH/PUT /orders/1.json
   def update
-    respond_to do |format|
-      if @order.update(order_params)
-        format.html { redirect_to @order, notice: 'Order was successfully updated.' }
-        format.json { render :show, status: :ok, location: @order }
-      else
-        format.html { render :edit }
-        format.json { render json: @order.errors, status: :unprocessable_entity }
-      end
-    end
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    render "update"
   end
 
   # DELETE /orders/1
@@ -76,6 +71,6 @@ class OrdersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
-      params.require(:order).permit(:title, :body)
+      params.require(:order).permit(:taobao_color_size)
     end
 end
