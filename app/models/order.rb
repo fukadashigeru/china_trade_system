9be3b72e4fade301e95a2088a5f888c72571a67d
fukadashigeru <sakusaku721@gmail.com>
2020-02-01 23:30:07 +0900

    OrderとTaobaoColorSizeのタオバオの外部キー制約とhas_oneを実装

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,6 +6,7 @@ class Order < ApplicationRecord
   has_many :user_orders
   has_many :users, through: :user_orders
   has_many :pictures
+  has_one :taobao_color_size
   belongs_to :japanese_retailer, class_name: 'User'
   belongs_to :chinese_buyer, class_name: 'User'
 
