5843427546d58c4f4cd646cdde656cb84b1d45b0
fukadashigeru <sakusaku721@gmail.com>
2020-03-15 15:21:17 +0900

    必須項目を入力したらチェックを入れる仕様追加

--- app/controllers/order_manual_inputs_controller.rb
+++ app/controllers/order_manual_inputs_controller.rb
@@ -1,6 +1,27 @@
 class OrderManualInputsController < ApplicationController
   def new
-    @order = current_user.japanese_retailer_orders.build
-    binding.pry
+    @orders = 3.times.map { current_user.japanese_retailer_orders.build }
   end
+
+  def create
+    params[:submit_true].keys.each do |i|
+      current_user.japanese_retailer_orders.create(order_params(i))
+    end
+    redirect_to "/"
+  end
+
+  private
+    def order_params(i)
+      params.require(i).permit(
+        :trade_no,
+        :postal,
+        :address,
+        :customer_name,
+        :phone,
+        :color_size,
+        :quantity,
+        :customer_remark,
+        :japanese_retailer_remark,
+      ).merge(chinese_buyer_id: params[:chinese_buyer_id])
+    end
 end
