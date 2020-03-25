4114a859ee84bad4284a54f0ee56d9ce403c067e
fukadashigeru <sakusaku721@gmail.com>
2020-03-25 23:32:42 +0900

    TaobaoUrl追加フォーム更新

--- app/controllers/item_sets_controller.rb
+++ app/controllers/item_sets_controller.rb
@@ -8,41 +8,12 @@ class ItemSetsController < ApplicationController
     item_set = current_user.item_sets.find(params[:id])
     begin
       ActiveRecord::Base.transaction do
-        item_set.processing_item_units_and_taobao_urls(taobao_url_params, first_candidate_params, have_stock_params, current_user)
-        # taobao_url_params.keys.each do |i|
-        #   taobao_url_params[i].keys.each do |j|
-        #     if j == "0"
-        #       item_unit = item_set.item_units.create
-        #     else
-        #       item_unit = item_set.item_units.find(j.to_i)
-        #     end
-        #     taobao_url_params[i][j].keys.each do |k|
-        #       taobao_url_params[i][j][k].keys.each do |l|
-        #         taobao_url_url = taobao_url_params[i][j][k][l]
-        #         ## TODO 空欄でも削除とかせな
-        #         next unless taobao_url_params[i][j][k][l].present?
-        #         if l == "0"
-        #           taobao_url = current_user.taobao_urls.find_or_create_by(url: taobao_url_url)
-        #           item_unit.item_unit_taobao_urls.create(taobao_url_id: taobao_url.id)
-        #         else
-        #           taobao_url = item_unit.taobao_urls.find(l.to_i)
-        #           item_unit_taobao_url = ItemUnitTaobaoUrl.find_by(item_unit_id: j.to_i , taobao_url_id: l.to_i)
-        #           if taobao_url.item_units.length > 1
-        #             taobao_url = current_user.taobao_urls.find_or_create_by(url: taobao_url_url)
-        #             item_unit_taobao_url.update(taobao_url_id: taobao_url.id)
-        #           else
-        #             taobao_url.update(url: taobao_url_url)
-        #           end
-        #         end
-        #         if k == first_candidate_params
-        #           binding.pry
-        #           item_unit.update(first_candidate_id: taobao_url.id)
-        #         end
-        #         taobao_url.update(is_have_stock: have_stock_params[k].to_i)
-        #       end
-        #     end
-        #   end
-        # end
+        item_set.processing_item_units_and_taobao_urls(
+          taobao_url_params,
+          first_candidate_params,
+          have_stock_params,
+          current_user
+        )
       end
       flash[:success] = "更新できました"
       redirect_to root_path
