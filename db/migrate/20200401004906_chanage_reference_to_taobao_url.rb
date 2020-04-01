c4bddbbc8a39e285a12fff4740639b71185c25a3
fukadashigeru <sakusaku721@gmail.com>
2020-04-01 09:51:08 +0900

    TaobaoUrlの親変更

--- db/migrate/20200401004906_chanage_reference_to_taobao_url.rb
+++ db/migrate/20200401004906_chanage_reference_to_taobao_url.rb
@@ -1,4 +1,6 @@
 class ChanageReferenceToTaobaoUrl < ActiveRecord::Migration[5.2]
   def change
+    remove_reference :taobao_urls, :user, foreign_key: true
+    add_reference :taobao_urls, :company, foreign_key: true
   end
 end
