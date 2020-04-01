c2dc800c7a9d86bf605981a9a3b907d1b3f6a652
fukadashigeru <sakusaku721@gmail.com>
2020-04-01 23:16:09 +0900

    招待機能

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,11 +1,12 @@
 class CompaniesController < ApplicationController
   def index
     session[:current_company_id] = nil
-    @owner_company_users = current_user.company_users.includes(:user).select{|x| x.role == 0}
+    @owner_company_users = current_user.company_users.includes(:user).select{|x| x.role == "owner"}
+    @belong_company_users = current_user.company_users.includes(:user).select{|x| x.role != "owner"}
   end
 
   def new
-    @company = @current_user.companies.build
+    @company = current_user.companies.build
   end
 
   def create
@@ -23,6 +24,13 @@ class CompaniesController < ApplicationController
     end
   end
 
+  def edit
+    @company =  current_user.companies.find(params[:id])
+  end
+
+  def update
+  end
+
   def login
     current_company = current_user.companies.find(params.require(:company_id))
     if current_company
@@ -34,6 +42,22 @@ class CompaniesController < ApplicationController
 
   def logout
     session[:current_company_id] = nil
+    redirect_to companies_path
+  end
+
+  def new_invite_user
+    @company = current_user.companies.find(params[:company_id])
+  end
+
+  def create_invite_user
+    user = User.find_by(email: params[:email])
+    company = current_user.companies.find(params[:company_id])
+    company_users = company.company_users.find_by(user_id: user.id)
+    if company_users
+      company_users.update(role: params[:role].to_i)
+    else
+      company.company_users.create(user_id: user.id, role: params[:role].to_i)
+    end
     redirect_to companies_path
   end
 
--- app/models/company.rb
+++ app/models/company.rb
@@ -7,9 +7,15 @@ class Company < ApplicationRecord
   has_many :japanese_retailer_orders, class_name: 'Order', :foreign_key => 'japanese_retailer_id'
   has_many :chinese_buyer_orders, class_name: 'Order', :foreign_key => 'chinese_buyer_id'
   accepts_nested_attributes_for :japanese_retailer_orders
-
+  accepts_nested_attributes_for :users
+  accepts_nested_attributes_for :company_users
+  
+  def owner
+    owner_company_users = company_users.select{|x| x.role == "owner"}
+    owner_company_users.map(&:user)
+  end
   def members
-    members_company_users = company_users.select{|x| x.role != 0}
-    members_company_users.map(&:users)
+    members_company_users = company_users.select{|x| x.role != "owner"}
+    members_company_users.map(&:user)
   end
 end
