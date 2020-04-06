fe10565e10a48004ff9f0da0ae8f69b7f88ca9aa
fukadashigeru <sakusaku721@gmail.com>
2020-04-06 23:51:30 +0900

    招待処理ほぼほぼfix

--- app/models/invited_company_user.rb
+++ app/models/invited_company_user.rb
@@ -1,4 +1,14 @@
 class InvitedCompanyUser < ApplicationRecord
   belongs_to :company
   belongs_to :user
+
+  def validation_save
+    invited_company_user = InvitedCompanyUser.find_by(user_id: user_id, company_id: company_id)
+    company_user = CompanyUser.find_by(user_id: user_id, company_id: company_id)
+    if invited_company_user.nil? && company_user.nil?
+      save
+    else
+      raise "Invalid Error"
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :confirmable
         #  :authentication_keys => [:login], invite_for: 24.hours
   # devise :database_authenticatable, :registerable, :recoverable,
   #        :trackable, :validatable, :confirmable, :lockable,
