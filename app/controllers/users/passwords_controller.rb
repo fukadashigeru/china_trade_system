6366e1ac575d78cc99c89fe019dce70057920fa7
fukadashigeru <sakusaku721@gmail.com>
2020-04-09 02:02:30 +0900

    パスワード忘れた場合の処理の場合分け追加

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -5,9 +5,18 @@ class Users::PasswordsController < Devise::PasswordsController
   # end
 
   # POST /resource/password
-  # def create
-  #   super
-  # end
+  def create
+    user = User.find_by(email:params[:user][:email])
+    if user&.invitation_token.present?
+      flash[:danger] = "保留中の招待からアカウントを作成してください。"
+      redirect_to new_user_password_path
+    elsif !user.confirmed?
+      flash[:danger] = "本登録を完了してください。"
+      redirect_to new_user_password_path
+    else
+      super
+    end
+  end
 
   # GET /resource/password/edit?reset_password_token=abcdef
   # def edit
