66fa855d28ab010944bcd03a77ea1a2404c64424
fukadashigeru <sakusaku721@gmail.com>
2020-04-08 15:20:45 +0900

    登録用認証メールの再送依頼後、thanks画面に飛ぶ処理追加

--- app/controllers/users/confirmations_controller.rb
+++ app/controllers/users/confirmations_controller.rb
@@ -21,8 +21,8 @@ class Users::ConfirmationsController < Devise::ConfirmationsController
     end
   end
 
-  # The path used after sending reset password instructions
-  def after_sending_reset_password_instructions_path_for(resource_name)
+  # The path used after resending confirmation instructions.
+  def after_resending_confirmation_instructions_path_for(resource_name)
     thanks_path
   end
 
