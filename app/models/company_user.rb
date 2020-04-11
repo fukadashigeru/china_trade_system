3b31e958e6dd60d5ce6153cee28a58a71b4a799d
fukadashigeru <sakusaku721@gmail.com>
2020-04-11 14:09:27 +0900

    [invited_company_usersをcompany_usersに集約する件]
    company_userモデルのcompanyとuserをuniqueにする
    
    オーナーの場合の招待マークの非表示

--- app/models/company_user.rb
+++ app/models/company_user.rb
@@ -2,4 +2,6 @@ class CompanyUser < ApplicationRecord
   belongs_to :user
   belongs_to :company
   enum role: { owner: 0, manager: 1, staff: 2}
+
+  validates :user_id, uniqueness: { scope: :company_id }
 end
